139b7ba3fe69f080591670212714a1ac871eaeeb
sainipiyush79 <psaini22u.rochester.edu>
2023-04-10 16:20:30 -0400

    Project 1 completed

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MainPage from './MainPage';
+import Navbar from './Navbar';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainPage />
+      
+    </div>
+  );
+}
+
+export default App;
